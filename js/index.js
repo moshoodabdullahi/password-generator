d6cb1ed008127044af7447f68b4ce2e201a627bd
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-07-03 18:43:03 +0100

    fix: Fix prettier error

--- js/index.js
+++ js/index.js
@@ -6,5 +6,3 @@ generatePasswordButton.addEventListener('click', function generatePassword() {
   passOneElement.textContent = Number(passOneElement.textContent) + 1;
   passTwoElement.textContent = Number(passTwoElement.textContent) + 2;
 });
-
-const passwordLength = 15

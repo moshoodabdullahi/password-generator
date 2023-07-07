5a64846559082490ee30f95179c90cfbdc4f342d
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-07-07 06:44:05 +0100

    feat: Add functiion that will loop 15 times

--- js/index.js
+++ js/index.js
@@ -1,8 +1,119 @@
+const characters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '~',
+  '`',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '-',
+  '+',
+  '=',
+  '{',
+  '[',
+  '}',
+  ']',
+  ',',
+  '|',
+  ':',
+  ';',
+  '<',
+  '>',
+  '.',
+  '?',
+  '/',
+];
+
 const passOneElement = document.querySelector('#generated-password-one');
 const passTwoElement = document.querySelector('#generated-password-two');
 const generatePasswordButton = document.querySelector('#generate-password');
 
 generatePasswordButton.addEventListener('click', function generatePassword() {
-  passOneElement.textContent = Number(passOneElement.textContent) + 1;
-  passTwoElement.textContent = Number(passTwoElement.textContent) + 2;
+  passOneElement.textContent = getRandomCharacters();
+  passTwoElement.textContent = getRandomCharacters();
 });
+
+getRandomCharacter = () => {
+  const randomCharacters = Math.floor(Math.random() * characters.length);
+  return characters[randomCharacters];
+};
+
+getRandomCharacters = () => {
+  let i = 0;
+  const number = 15;
+  let randomCharacters = '';
+
+  while (i < number) {
+    randomCharacters += getRandomCharacter();
+    i += 1;
+  }
+  return randomCharacters;
+};
